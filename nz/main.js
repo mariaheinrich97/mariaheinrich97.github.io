df6d8004db8ead4ac3d7473beeabc67ff2132967
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-07 15:57:29 +0200

    Arrey einarbeiten bei Koordinaten

--- nz/main.js
+++ nz/main.js
@@ -7,7 +7,16 @@ let lat = -44.697222;
 let lng = 169.135278;
 let zoom = 11;
 
-let map = L.map('map').setView([lat, lng], zoom);
+let coords = [-44.70, 175.65];
+console.log(coords)
+console.log(coords[0]) 
+console.log(coords[1]) 
+console.log(coords.length)
+//arrey hat immer versch. / mehrere Elemente und steht in [] 
+ //console.log() steht im Reiter unten
+ //Wert 1 in der Reihe = 0
+ 
+let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
