c006d4621550476bb6433df4b38bfc84b9fad064
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-07 16:08:00 +0200

    Coordinaten in Text

--- nz/main.js
+++ nz/main.js
@@ -15,12 +15,18 @@ console.log(coords.length)
 //arrey hat immer versch. / mehrere Elemente und steht in [] 
  //console.log() steht im Reiter unten
  //Wert 1 in der Reihe = 0
+
+ console.log('text');
+ console.log("text");
+ console.log('id="map"'); //Grund für unterschied ' & " - wenn ich im Ausdruck " oder ' verwenden möchte
+ console.log(`text "Variablen" 'Auflösen' latitute = ${lat}`) // Backtips wichtig, da ich dann ' & " im Text verwenden kann und Variablen auflösen kann
  
 let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
 
 L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Wanaka</h3>')
