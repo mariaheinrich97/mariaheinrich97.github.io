1417732e93cc8b833a992ec969a427627d217c40
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-07 15:45:08 +0200

    let für Koordinaten

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,18 @@
 /*Neuseelandreise Skript */
 // einzeiliger Kommentar
 
-var map = L.map('map').setView([-44.697222, 169.135278], 10);
+// Variablen für Koordinaten vereinfachen Änderungen
+
+let lat = -44.697222;
+let lng = 169.135278;
+let zoom = 11;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-44.697222, 169.135278]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Wanaka</h3>')
     .openPopup();
