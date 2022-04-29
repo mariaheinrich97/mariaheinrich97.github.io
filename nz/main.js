463d79c36de6422d2e80d9fe669764941d8f14ea
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-29 20:02:41 +0200

    main.js auf Hütte Radius

--- nz/main.js
+++ nz/main.js
@@ -1,63 +1,95 @@
-/* OGD Wien Beispiel */
+/* mehrzeiliger Kommentar */
+// einzeiliger Kommentar
+
+let zoom = 11;
+
+let coords = [ETAPPEN[6].lat, ETAPPEN[6].lng];
+
+/*
+console.log(coords)
+console.log(coords[0])
+console.log(coords[1])
+console.log(coords.length)
+//arrey hat immer versch. / mehrere Elemente und steht in [] 
+//console.log() steht im Reiter unten
+//Wert 1 in der Reihe = 0
+
+console.log('text');
+console.log("text");
+console.log('id="map"'); //Grund für unterschied ' & " - wenn ich im Ausdruck " oder ' verwenden möchte
+console.log(`text "Variablen" 'Auflösen' latitute = ${lat}`) // Backtips wichtig, da ich dann ' & " im Text verwenden kann und Variablen auflösen kann
+console.log(ETAPPEN);
+console.log(ETAPPEN[0]);
+console.log(ETAPPEN[0].nr)
+console.log(ETAPPEN[0].github)
+console.log(ETAPPEN[0].titel)
+console.log(ETAPPEN[0].wikipedia)
+console.log(ETAPPEN[0].lat)
+console.log(ETAPPEN[0].lng)
+*/
 
 
-let stephansdom = {
-    lat: 48.208493,
-    lng: 16.373118,
-    title: "Stephansdom",
-};
+let map = L.map('map').setView(coords, zoom);
 
-let startLayer = L.tileLayer.provider("BasemapAT.grau")
-// L
-//tileLayer - ein Layer
-//providers - genaue Kartenteile
-
-let map = L.map("map", {
-    center: [stephansdom.lat, stephansdom.lng],
-    zoom: 12,
-    layers: [
-        startLayer
-    ]
-})
-
-let layerControl = L.control.layers({
-    "BasemapAT Grau": startLayer,
-    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
-    "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
-    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
-    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
-    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
-    "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-    "Basemap mit Orthofoto und Beschriftung": L.layerGroup([
-        L.tileLayer.provider("BasemapAT.orthofoto"),
-        L.tileLayer.provider("BasemapAT.overlay"),
-    ])
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-layerControl.expand();
+//Arrey in etappen.js für Labeling und Informationen der einzelnen Etappen, die hier aufgerufen werden können und die Label werden in einer for-Schleife abgerufen
 
-let sightLayer = L.featureGroup();
+for (let etappe of ETAPPEN) {
+    let popup =
+        `<h3>${etappe.titel} (Etappe ${etappe.nr})</h3>
+     <ul>
+     <li> geogr. Länge: ${etappe.lng}</li>
+     <li> geogr. Breite: ${etappe.lat} </li>
+     <li> <a href= "${etappe.wikipedia}">Link zur Wikipediaseite</a> </li>
+     <li> <a href= "${etappe.github}.github.io/nz/">Link zur GitHub-Seite</a> </li>
+     </ul>`;
+    //console.log(etappe)
+    let navClass = "etappenLink";
 
-layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+    let mrk = L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
+    if (etappe.nr == 6) {
+        mrk.openPopup();
+        navClass = "etappenLink etappeAktuell"
+    }
 
-let mrk = L.marker([stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
+    // = Zuweisung == abfrage, ob etwas Gleich ist, wie ... 
+    //Etappennavigation erweitern - Link zu Etappen einfügen
+    //document.querySelector verbindet zur html. Seite & sucht nach navigation
+    // innerHTML sucht innerhalb von HTML und setzt ihn ="xy" --> xy steht unter der Karte 
 
-sightLayer.addTo(map);
+    let link = `<a href= "https://${etappe.github}.github.io/nz/"
+    class="${navClass} " title="${etappe.titel}">${etappe.nr}</a>`;
+    document.querySelector("#navigation").innerHTML += link;
+}
+// HUTS
+for (let hut of HUTS) {
 
-// Maßstab hinzufügen
-L.control.scale({
-    imperial: false,
-}).addTo(map);
+    let popup =
+        `<h3>${hut.name}</h3>
+    <h4>Region: ${hut.region}</h4>
+    <hr> 
+    <p>${hut.info}<p>
+    <img src="${hut.image}" alt="Bild der Hütte ${hut.name}">
+    <hr> 
+    <a href="${hut.link}" target="Neuseeland">Link zur Hütte</a>
+    `
 
-// z. B. für Smartphones gut
-L.control.fullscreen().addTo(map);
-// map.addControl(L.control.fullscreen());
+    let statusColor;
+    if (hut.open == true) {
+        statusColor = "green";
+    } else {
+        statusColor = "red";
+    }
+    L.circleMarker([hut.lat, hut.lng], {
+            color: statusColor,
+            radius: 5
+        }).addTo(map)
+        .bindPopup(popup)
+}
 
-// miniMap = Übersichtskarte
-let miniMap = new L.Control.MiniMap(
-    L.tileLayer.provider("BasemapAT"),
-).addTo(map);
 
-// Karten
-// Scale
-// MiniMap
+//keine ; da im Optionen-Objekt
+// ${} nur im ""
