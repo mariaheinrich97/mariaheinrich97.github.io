0ef5c10e8ffbba3c1c23947cc0e0fee7d6481735
Maria <maria.heinrich@student.uibk.ac.at>
2022-05-01 11:15:45 +0200

    Ausgeklammerter Versuch Layer Plugin

--- nz/main.js
+++ nz/main.js
@@ -4,9 +4,9 @@ let zoom = 11;
 
 let coords = [ETAPPEN[6].lat, ETAPPEN[6].lng];
 
+// funktionierender Layer (ohne Leaflet Plugin)
 let map = L.map('map').setView(coords, zoom);
 
-// funktionierender Layer (ohne Leaflet Plugin)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -22,7 +22,6 @@ for (let etappe of ETAPPEN) {
      <li> <a href= "${etappe.wikipedia}">Link zur Wikipediaseite</a> </li>
      <li> <a href= "${etappe.github}.github.io/nz/">Link zur GitHub-Seite</a> </li>
      </ul>`;
-    //console.log(etappe)
     let navClass = "etappenLink";
 
     let mrk = L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
@@ -66,6 +65,94 @@ for (let hut of HUTS) {
         .bindPopup(popup)
 }
 
+/* ----------------------------------------------------------------------------------------
+// Versuch Leaflet Plugin
+
+let startLayer = L.tileLayer.provider("Esri.WorldStreetMap");
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+})
+//Layer einbinden
+
+let layerControl = L.control.layers({
+    "Esri.WorldStreetMap": startLayer,
+    "Esri.WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+}).addTo(map);
+
+layerControl.expand();
+
+let sightLayer = L.featureGroup();
+
+layerControl.addOverlay(sightLayer, "Hütten&Etappen");
+
+//Arrey in etappen.js für Labeling und Informationen der einzelnen Etappen, die hier aufgerufen werden können und die Label werden in einer for-Schleife abgerufen
+
+for (let etappe of ETAPPEN) {
+    let popup =
+        `<h3>${etappe.titel} (Etappe ${etappe.nr})</h3>
+     <ul>
+     <li> geogr. Länge: ${etappe.lng}</li>
+     <li> geogr. Breite: ${etappe.lat} </li>
+     <li> <a href= "${etappe.wikipedia}">Link zur Wikipediaseite</a> </li>
+     <li> <a href= "${etappe.github}.github.io/nz/">Link zur GitHub-Seite</a> </li>
+     </ul>`;
+    let navClass = "etappenLink";
+
+    let mrk = L.marker([etappe.lat, etappe.lng]).addTo(sightLayer).bindPopup(popup);
+    sightLayer.addTo(map);
+    if (etappe.nr == 6) {
+        mrk.openPopup();
+        navClass = "etappenLink etappeAktuell"
+    }
+
+    // = Zuweisung == abfrage, ob etwas Gleich ist, wie ... 
+    //Etappennavigation erweitern - Link zu Etappen einfügen
+    //document.querySelector verbindet zur html. Seite & sucht nach navigation
+    // innerHTML sucht innerhalb von HTML und setzt ihn ="xy" --> xy steht unter der Karte 
+
+    let link = `<a href= "https://${etappe.github}.github.io/nz/"
+    class="${navClass} " title="${etappe.titel}">${etappe.nr}</a>`;
+    document.querySelector("#navigation").innerHTML += link;
+}
+// HUTS einfügen 
+for (let hut of HUTS) {
+
+    let popup =
+        `<h3>${hut.name}</h3>
+    <h4>Region: ${hut.region}</h4>
+    <hr> 
+    <p>${hut.info}<p>
+    <img src="${hut.image}" alt="Bild der Hütte ${hut.name}">
+    <hr> 
+    <a href="${hut.link}" target="Neuseeland">Link zur Hütte</a>
+    `
+
+    let statusColor;
+    if (hut.open == true) {
+        statusColor = "green";
+    } else {
+        statusColor = "red";
+    }
+    L.circleMarker([hut.lat, hut.lng], {
+            color: statusColor,
+            radius: 5
+        }).addTo(sightLayer)
+        .bindPopup(popup)
+        sightLayer.addTo(map);
+}
+
+// ENDE VERSUCHE LEAFLET PLUGIN
+
+
+
+-----------------------------------------------------------------------------
+*/
 
 // miniMap = Übersichtskarte
 let miniMap = new L.Control.MiniMap(
@@ -78,7 +165,6 @@ let miniMap = new L.Control.MiniMap(
 L.control.scale({
     imperial: false,
 }).addTo(map);
-
 
 // Scale z. B. für Smartphones gut
 L.control.fullscreen().addTo(map);
